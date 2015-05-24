ff485c93b409e32af19f15b5843670dc97e6c9a3
Camail <adam.borgo@valpo.edu>
2015-05-24 01:07:14 -0500

    overhaul on pony.py
    completely changed .whenispony and have added .ponyvideo

--- pony.py
+++ pony.py
@@ -5,64 +5,124 @@ Pony commands written to make DoT` happy
 
 from urllib2 import urlopen, Request
 from datetime import datetime, timedelta
-import re
 from util import hook
 import random
+import json
+import sqlite3
 
-regex = re.compile(r"=.+]")
 
-class Episode:
+class Episode(object):
     def __init__(self, data):
         self.data = data
-        info = regex.search(data).group().split(',')
-        info[0] = info[0]+','+info[1]
-        info.pop(1)
-        self.date = info[0][12:-2]
-        self.season = info[1]
-        self.episode = info[2]
-        self.title = info[3].rstrip(']')
-        self.date = datetime.strptime(self.date,"%B %d, %Y %H:%M:%S")
+        self._date = data["time"][:-5] #removes ".000z"
+        self.date = datetime.strptime(data["time"][:-5],"%Y-%m-%dT%H:%M:%S")
+        self.season = data["season"]
+        self.episode = data["episode"]
+        self.title = data["name"]
         
+def db_init(db):
+    "check to see that our db has the ponyepisodes table"
+    db.execute("create table if not exists ponyepisodes"
+               "(title, season, episode, date, video,"
+               "primary key(title))")
+    db.commit()
+    return db
 
-def ponyget():
-    req = Request('http://ponycountdown.com/api/data.js',
+def ponytable(db=None):
+    """Populates table with episodes that are not in it already."""
+    db_init(db)    
+    req = Request('http://api.ponycountdown.com/',
                   headers={'User-Agent' : "Magic Browser"})
-    ep_lst = urlopen(req).read().split(';')
+    ep_lst = json.load(urlopen(req))
     episodes = []
-    for data in ep_lst[1:-1]:
-        episode = Episode(data)
-        episodes.append(episode)
-    return episodes
+    for i in ep_lst:
+        ep = Episode(i)
+        episodes.append(ep)
+    sql_select = 'select title from ponyepisodes where title=?'
+    sql_insert = 'insert into ponyepisodes values(?, ?, ?, ?, ?)'
+    for episode in episodes:
+        found = db.execute(sql_select, (episode.title,)).fetchone()
+        db.commit()
+        if found:
+            continue
+        db.execute(sql_insert,(episode.title, episode.season,
+                                 episode.episode, episode._date, ''))
+        db.commit()
+        
 
 
 @hook.command()
 def whenispony(inp):
-    episodes = ponyget()
+    req = Request('http://api.ponycountdown.com/next',
+                  headers={'User-Agent' : "Magic Browser"})
+    ep = json.load(urlopen(req))
+    if ep == None:
+        return "I don't know...T_T"
+    
+    episode = Episode(ep)
     now = datetime.utcnow()
-    seven = timedelta(days=7)
-    zero = timedelta(days=0)
-    found = False
-    for episode in episodes[::-1]:
-        diff = episode.date-now
-        if diff >= zero and diff <= seven:
-            days = diff.days
-            hours = diff.seconds//3600
-            minutes = (diff.seconds-hours*3600)//60
-            seconds = (diff.seconds-(hours*3600+minutes*60))
-            times = []
-            for time in zip([days, hours, minutes, seconds],
-                            [' day',' hour',' minute',' second']):
-                if time[0] == 1:
-                    times.append(str(time[0])+time[1])
-                else:
-                    times.append(str(time[0])+time[1]+'s')
+    diff = episode.date-now
+    days = diff.days
+    hours = diff.seconds//3600
+    minutes = (diff.seconds-hours*3600)//60
+    seconds = (diff.seconds-(hours*3600+minutes*60))
+    times = []
+    
+    for time in zip([days, hours, minutes, seconds],
+                    [' day',' hour',' minute',' second']):
+        if time[0] == 1:
+            times.append(str(time[0])+time[1])
+        else:
+            times.append(str(time[0])+time[1]+'s')
             
  
-            fmt = "{}, {}, {}, and {} from pony!"
-            return fmt.format(*times)
+    fmt = "{}, {}, {}, and {} from pony!"
+    return fmt.format(*times)
+    
 
 @hook.command()
 def ponyRandom(inp):
     pony_list = ['Pinkie Pie', ' Twilight Sparkle', ' Rainbow Dash', ' Rarity',
                  ' Applejack', ' Spike', ' Fluttershy']
     return random.choice(pony_list)
+
+@hook.command()
+def ponyVideo(inp, db=None):
+    """.ponyVideo + s00e00 <video> -- adds a video to an episode |||
+.ponyVideo find s00e00 -- retrieves video for an episode
+"""
+    ponytable(db)
+    inp = inp.lower()
+    if inp[:4] == "find":
+        try:
+            season, episode = (int(inp[6:8].lstrip('0')),
+                               int(inp[9:11].lstrip('0')))
+        except ValueError:
+            return "Incorrect Syntax for season or episode"
+        sql_select = "select video from ponyepisodes where season=? and episode=?"
+        video = db.execute(sql_select, (season, episode)).fetchone()
+        db.commit()
+        return video[0]
+    if inp[:1] == "+":
+        _id = inp[1:8]
+        try:
+            season, episode = (int(inp[3:5].lstrip('0')),
+                               int(inp[6:8].lstrip('0')))
+        except ValueError:
+            return "Incorrect Syntax for season or episode"
+        video = inp[9:]
+        verify = db.execute('select * from ponyepisodes where season=? and episode=?',
+                   (season, episode)).fetchall()
+        if not verify:
+            return "I couldn't find that episode!"
+        
+        sql_update = ("update ponyepisodes set video=? "
+                     "where season=? and episode=?")
+        db.execute(sql_update,(video,season,episode))
+        db.commit()
+        
+        return "The video for s{0:02}e{1:02} is now {2}.".format(season,episode,video)
+
+    return "what?"
+
+          
